df3dc2296fbbea42e38001da955fd47e8f716a99
megabizz4 <megabizz4@gmail.com>
2025-09-29 01:01:20 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,22 +1,3 @@
 HTML = """
-It was windy and my hair was blowing all over my face.
-The two kids snuck into the principal's office after school.
-She wished she had a key that could take her anywhere.
-His appearance terrified her and she screamed.
-Tom started a list so he wouldn't forget anything.
-The earthquake was the scariest one that we had ever experienced.
-There was a big fire near my house last night.
-She found little pellets of iron in her garden bed.
-Last weekend, she wanted to go to the wedding, but she wasn't sure it was safe.
-Her hat was green, her mittens were yellow, and her coat was orange.
-Tom can't forget the time he and Mary had their first argument.
-The conversion rate of USD to Chinese RMB is approximately 1:6.
-The tree was so old that it was considered a historic monument.
-There is nothing about the situation I could relate to.
-I was excited to spend time with my wife without being interrupted by kids.
-She liked Dune, but it didn't pass the Bechdel Test.
-Let's all just take a moment to breathe, please!
-That would be a really big surprise, wouldn't it?
-What would you change about yourself if you could change one thing?
 Are you honestly saying you believe in ghosts?
 """
